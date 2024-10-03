13ccb5dddc1dba2cabfe22a46004dd22029e9de4
hulj07001 <hulj07001@E-PW05CGR7>
2024-10-03 12:34:11 +0200

    Add project files.

---
+++ Övninguppgifter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace övninguppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vilket årtal är det? ");
+            string årtaltext=Console.ReadLine();
+            int årtal=int.Parse(årtaltext);
+            int omår = 20;
+            // Man kan ändra värdet på "omår" beroende på hur många år efter man vill veta
+            
+            int årtalom = årtal + omår;
+            Console.WriteLine("Om " + omår + " år kommer prtalet vara " + årtalom + ".");
+        }
+    }
+}
